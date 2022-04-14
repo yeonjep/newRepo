e0a345e1cbcb0685568844e806db130ab84d21bf
yeonjep <yeonje1004@naver.com>
2022-04-14 02:56:55 -0700

    wordfreq

---
+++ word_freq.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import sys
+
+textfile=open('textfile.txt','r')
+
+dic={}
+
+
+lines=textfile.readlines()
+
+for i in range (len(lines)) :
+
+   word=lines[i].split()
+   for it in word:
+      it.lower()
+
+      if it in dic:
+         dic[it]+=1
+      else:
+         dic[it]=1
+
+
+for i,j in dic.items():
+
+   print(i,j)
+
